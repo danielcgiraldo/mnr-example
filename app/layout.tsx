2a2b61ce661a23dd9308e252407db8379c808fee
Daniel Castillo Giraldo <dc.giraldocg@hotmail.com>
2023-01-18 00:51:24 -0500

    Head Contents Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,26 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-  
+  const description: string = '';
+  const title: string = '';
   return (
     <html lang="es">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content={description} />
+        <meta name="keywords" content="" />
+        <meta name="author" content="" />
+        <meta name="robots" content="index, follow" />
+
+        <meta property="og:description" content={description} />
+        <meta property="og:title" content={title} />
+
+        <meta name="twitter:description" content={description} />
+        <meta property="twitter:title" content={title} />
+
+        <title>{title}</title>
+      </head>
       <body>
         Root Layout
         {children}
